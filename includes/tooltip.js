1dc65856828b7a0314f67afdae16d01ecbb051e1
edwinchen85 <edwinchen85@gmail.com>
2017-03-02 14:30:32 +0800

    Add styling and functionality for detecting and setting html-based tooltip

--- includes/tooltip.js
+++ includes/tooltip.js
@@ -8,6 +8,12 @@ $(document).ready(function(){
       $('#tooltip_container').html( $(this).attr('data-tip-source'));
     }
 
+    if( $(this).attr('data-tip-type') == 'html' ) {
+      var elementToGet = '#' + $(this).attr('data-tip-source');
+      var newHTML = $(elementToGet).html();
+      $('#tooltip_container').html( newHTML );
+    }
+
   }).mouse.move(function(e){
 
   }).mouseout(function(e){
